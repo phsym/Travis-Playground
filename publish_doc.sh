fb86e7dabaeb37c48a1e712e6acfaae4f2b29dc1
pierresy <pierre-henri.symoneaux@alcatel-lucent.com>
2016-01-11 20:31:57 +0100

    fix

--- publish_doc.sh
+++ publish_doc.sh
@@ -11,19 +11,16 @@ RELEASE="master"
 
 cargo doc || exit 1
 
-# git branch -a
-# git fetch -a https://github.com/phsym/Travis-Playground.git
-# git checkout gh-pages
 git clone --depth=50 --branch=gh-pages https://github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 rm -Rf "gh-pages/${RELEASE}"
 mkdir "gh-pages/${RELEASE}"
 
-echo "<meta http-equiv=refresh content=0;url=test/index.html" > target/doc/index.html 
+echo "<meta http-equiv=refresh content=0;url=test/index.html>" > target/doc/index.html 
 cp -R target/doc/* "gh-pages/${RELEASE}/"
 
 cd gh-pages
-git config user.name "travis"
-git config user.email "none@travis-ci.org"
+# git config user.name "travis"
+# git config user.email "none@travis-ci.org"
 
 git add --all
 git commit -m "Updated documentation"
