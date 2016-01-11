8012d7a2225cf9b887d89cf344dc8f336285bdc6
travis-ci <travis@travis-ci.org>
2016-01-11 19:54:41 +0000

    Updated documentation

--- master/search-index.js
+++ master/search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['test'] = {"items":[[0,"","test","Test crate",null,null],[5,"hello_world","","Say Hello to the world",null,{"inputs":[],"output":null}]],"paths":[]};
+searchIndex['test'] = {"items":[[0,"","test","Test crate",null,null],[5,"hello_world","","Say Hello to the world !!!",null,{"inputs":[],"output":null}]],"paths":[]};
 initSearch(searchIndex);
