92d1b9b88622be90651a8493c71e7cb31d911443
pierresy <pierre-henri.symoneaux@alcatel-lucent.com>
2016-01-11 20:02:52 +0100

    ...

--- publish_doc.sh
+++ publish_doc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Rust version : ${TRAVIS_RUST_VERSION}"
+
 [ $TRAVIS_PULL_REQUEST = false ] || exit 0
 [ $TRAVIS_BRANCH = master ] || exit 0
 
@@ -7,7 +9,7 @@ RELEASE="master"
 
 [ -z $TRAVIS_TAG ] || RELEASE=$TRAVIS_TAG
 
-cargo doc
+cargo doc || exit 1
 
 git checkout gh-pages
 rm -Rf "${RELEASE}"
@@ -15,8 +17,12 @@ mkdir "${RELEASE}"
 
 echo "<meta http-equiv=refresh content=0;url=test/index.html" > target/doc/index.html 
 cp -R target/doc/* "${RELEASE}/"
+
+git config user.name "travis"
+git config user.email "none@travis-ci.org"
+
 git add --all
 git commit -m "Updated documentation"
 
-git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages || exit 1
 
