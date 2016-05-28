53bc667bee526cb7764e99c03039522183ec4e02
travis-ci <travis@travis-ci.org>
2016-05-28 17:16:18 +0000

    Updated documentation on master for c7bba031aec3fc5821588058fdd7eb4285da38db

--- master/search-index.js
+++ master/search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['test'] = {"items":[[0,"","test","Test crate that only prints \"Hello World\"",null,null],[5,"hello_world","","Say Hello to the world",null,{"inputs":[],"output":null}],[5,"main","","Program entry point",null,{"inputs":[],"output":null}]],"paths":[]};
+searchIndex["test"] = {"doc":"Test crate that only prints &quot;Hello World&quot;","items":[[5,"hello_world","test","Say Hello to the world",null,{"inputs":[],"output":null}],[5,"main","","Program entry point",null,{"inputs":[],"output":null}]],"paths":[]};
 initSearch(searchIndex);
